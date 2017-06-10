79fedcaa0083c57608f641e9153818424d4a00de
xucg1993 <805679287@qq.com>
2017-06-11 00:36:46 +0800

    更新

--- src/components/classification/commodity.vue
+++ src/components/classification/commodity.vue
@@ -4,8 +4,25 @@
   </div>
 </template>
 <script>
+  export default{
+    mounted() {
+      //直接打印不到
+      this.setUserInfo();
 
+      //加一定的延时就可以打印出来
+      let _this = this;
+      setTimeout(function() {
+        _this.setUserInfo();
+      }, 100)
+    },
+    methods :{
+      setUserInfo: function() {
+        let userInfo = this.$route.query;
+        console.log('进来')
+        console.log(userInfo)
+      }
+    }
+  }
 </script>
 <style scoped>
-
 </style>
