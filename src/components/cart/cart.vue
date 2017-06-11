c53604594fc1c0f6e477f82feda245cbc5f6326b
xucg1993 <805679287@qq.com>
2017-06-11 21:20:59 +0800

    更新

--- src/components/cart/cart.vue
+++ src/components/cart/cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="margin-top: 60px">
+  <div style="margin-top: 46px;margin-bottom: 64px">
     <group title="购物车">
       <x-table :cell-bordered="false" style="background-color:#fff;">
         <thead>
@@ -11,22 +11,32 @@
         </tr>
         </thead>
         <tbody>
+        <tr v-if="cartlist.length <= 0">
+          <td colspan="4" height="400px"><span class="kong">空空如也也，快去选购吧</span></td>
+        </tr>
         <tr  v-for="cart in cartlist" >
           <td>{{cart.foodname}}</td>
           <td>${{ (cart.foodprice * cart.weightnumber).toFixed(2) }}</td>
           <td>
-            <x-number v-model="cart.weightnumber" button-style="round" step="0.1" :min="0.5" :max="100"  @on-change="addCar(cart.goodsid,cart.foodprice,cart.weightnumber)"></x-number>
+            <x-number v-model="cart.weightnumber" button-style="round" step="0.1" :min="0.5" :max="100" fillable  @on-change="editCar(cart.goodsid,cart.foodprice,cart.weightnumber)"></x-number>
           </td>
           <td>删除</td>
         </tr>
         </tbody>
       </x-table>
     </group>
+    <box gap="10px 10px">
+      <flexbox>
+        <flexbox-item>
+          <x-button v-if="cartlist.length > 0" type="warn" @click.native="">提交订单</x-button>
+        </flexbox-item>
+      </flexbox>
+    </box>
   </div>
 </template>
 
 <script scoped>
-  import {Group, XNumber, XSwitch, XTable} from 'vux'
+  import {Group, XNumber, XSwitch, XTable,Box, Flexbox, FlexboxItem, XButton} from 'vux'
   var getCartListURL = 'http://192.168.199.117:8081/cart/getCartList.action';
   var insertCartURL = 'http://192.168.199.117:8081/cart/editCartList.action';
   export default {
@@ -34,7 +44,7 @@
       XNumber,
       Group,
       XSwitch,
-      XTable
+      XTable,Box, Flexbox, FlexboxItem, XButton
     },
     data () {
       return {
@@ -56,10 +66,7 @@
       change (val) {
         console.log('change', val)
       },
-      addCar: function (foodid,foodprice,weightnumber) {
-//        var foodid = this.$route.query.foodid;
-//        var roundValue = this.roundValue;
-//        var cleanfoodprice = this.cleanfoodprice;
+      editCar: function (foodid,foodprice,weightnumber) {
         var that = this;
         this.$http.post(insertCartURL + '?goodsid=' + foodid + '&weightnumber=' + weightnumber + '&price=' + (foodprice * weightnumber).toFixed(2)).then(function (res) {
           if (res.data.errorCode == 0){
@@ -78,3 +85,9 @@
     }
   }
 </script>
+<style>
+  .kong{
+   color: #a9a8a6;
+    font-size: 12px;
+  }
+</style>
